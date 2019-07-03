7e153034c2211c9d482fc86deb1eb2a92e05a969
Piotr Bernad <piotr.bernad@appunite.com>
2019-07-03 12:51:11 +0200

    Initial Commit

---
+++ PlacesPicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlacesPicker
+//
+//  Created by Piotr Bernad on 03/07/2019.
+//  Copyright © 2019 Piotr Bernad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PlacesPickerTests/PlacesPickerTests.swift
@@ -0,0 +1,34 @@
+//
+//  PlacesPickerTests.swift
+//  PlacesPickerTests
+//
+//  Created by Piotr Bernad on 03/07/2019.
+//  Copyright © 2019 Piotr Bernad. All rights reserved.
+//
+
+import XCTest
+@testable import PlacesPicker
+
+class PlacesPickerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
